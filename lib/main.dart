f4223f3118cce5a9844947841f7c39673749e7d3
kubaolszewski <kuba.olszewski.0704@icloud.com>
2024-05-21 22:24:38 +0200

    Initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list/app.dart';
+import 'package:todo_list/firebase_options.dart';
 
 void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
