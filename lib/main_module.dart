594c68f949d3bef1c3c86063a8f53af8d5f05edf
kubaolszewski <kuba.olszewski.0704@icloud.com>
2024-06-13 23:21:11 +0200

    Add DI modules and envied package

--- lib/main_module.dart
+++ lib/main_module.dart
@@ -2,13 +2,24 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 import 'common/extensions/string_ext.dart';
 import 'common/utils/app_paths/app_paths.dart';
+import 'di/di_bloc_module.dart';
+import 'di/di_data_module.dart';
+import 'di/di_network_module.dart';
+import 'di/di_repositories_module.dart';
+import 'di/di_services_module.dart';
 import 'features/account/account_module.dart';
 import 'features/home/home_module.dart';
 import 'features/search/search_module.dart';
 
 class MainModule extends Module {
   @override
-  void binds(i) {}
+  List<Module> get imports => [
+        DiBlocModule(),
+        DIDataModule(),
+        DiNetworkModule(),
+        DiRepositoriesModule(),
+        DiServicesModule(),
+      ];
 
   @override
   void routes(RouteManager r) {
